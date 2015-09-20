7664a1ba0f8ba8e68d735ccea5248cb98329667e
mshirdel <mshirdel@gmail.com>
2015-09-20 09:44:19 +0430

    adding test

--- polls/views.py
+++ polls/views.py
@@ -1,34 +1,34 @@
-from django.shortcuts import render, get_object_or_404
-from django.template import RequestContext, loader
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from .models import Question, Choice
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Choice, Question
 
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # method 1:
-    # template = loader.get_template('polls/index.html')
-    # context = RequestContext(request, {
-    #     'latest_question_list': latest_question_list,
-    # })
-    # return HttpResponse(template.render(context))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-    # method 2
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
